74fb891eb74884b7f4024b9ab7eb01f408618e19
Mark Wardle <mark@wardle.org>
2021-10-09 21:07:42 +0100

    Initial crude wrapper for testing.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# comprehend
-Natural language processing for SNOMED CT
+# comprehend : natural language processing for SNOMED CT
+
+This is a thin wrapper bringing together the Amazon Web Services (AWS) Comprehend Medical 
+service with a SNOMED terminology server, [hermes](https://github.com/wardle/hermes).
+
+At the moment, it is simply a library that is used by [pc4](https://github.com/wardle/pc4) but
+it would be straightforward to build this as an independent microservice, even
+if only for demonstration purposes initially!
---
+++ src/com/eldrix/comprehend/core.clj
@@ -0,0 +1,61 @@
+(ns com.eldrix.comprehend.core
+  (:require [cognitect.aws.client.api :as aws]
+            [cognitect.aws.credentials :as credentials]
+            [com.eldrix.hermes.core :as hermes]))
+
+(defn ^:private make-constraint [{:keys [Category Traits Type]}]
+  (cond
+    (= "MEDICAL_CONDITION" Category)
+    "<404684003"
+    (= "MEDICATION" Category)
+    "<373873005"
+    (= Category "TEST_TREATMENT_PROCEDURE")
+    "<71388002"
+    (= Category "ANATOMY")
+    "<123037004"))
+
+(defn ^:private entity->snomed [hermes {:keys [Text] :as entity}]
+  (let [ecl (make-constraint entity)]
+    (vector entity (hermes/search hermes {:s Text :constraint ecl :max-hits 1 :fallback-fuzzy 2}))))
+
+(defn simple-parse
+  "Parse free text for clinical conditions and medications."
+  [svc s]
+  (->> (aws/invoke (.-comprehend svc) {:op      :DetectEntitiesV2
+                                       :request {:Text s}})
+       :Entities
+       (filter #(#{"MEDICATION" "MEDICAL_CONDITION"} (:Category %))) ;; only keep medications and conditions
+       (remove #((set (map :Name (:Traits %))) "NEGATION")) ;; remove any with trait NEGATION
+       (map #(entity->snomed (.-hermes svc) %))))
+
+(deftype Svc [hermes comprehend])
+
+(defn open
+  "Open a comprehend service using the configuration specified.
+  Parameters:
+  - config - configuration including keys:
+           |- :hermes - a hermes terminology service
+           |- :access-key-id - access key for basic credentials
+           |- :secret-access-key - secret access key."
+  [{hermes            :com.eldrix/hermes
+    access-key-id     :access-key-id
+    secret-access-key :secret-access-key :as config}]
+  (->Svc hermes
+         (aws/client (cond-> (merge config {:api :comprehendmedical})
+                             (and access-key-id secret-access-key)
+                             (assoc :credentials-provider (credentials/basic-credentials-provider {:access-key-id     access-key-id
+                                                                                                   :secret-access-key secret-access-key}))))))
+
+(comment
+  (def comprehend (aws/client {:api :comprehendmedical}))
+  (def comprehend (aws/client {:api                  :comprehendmedical
+                               :credentials-provider (credentials/basic-credentials-provider
+                                                       {:access-key-id     "access-key"
+                                                        :secret-access-key "secret-key"})}))
+  (def svc (open {:com.eldrix/hermes hermes
+                  :access-key-id     "access-key"
+                   :secret-access-key "secret-key"} ))
+  svc
+
+  (keys (aws/ops comprehend))
+  (aws/validate-requests comprehend))
